a6964be45f0ff50b79e02582cf81acd093c830aa
Kenneth Carter <kccarter32@gmail.com>
2025-08-31 23:31:10 -0600

    no message

---
+++ ISubLighQueryProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace SubLight
+{
+    public interface ISubLighQueryProvider
+        : IQueryProvider
+    {
+        // Provider-specific query translation
+        string Translate(IQueryTranslator translator);
+    }
+}
